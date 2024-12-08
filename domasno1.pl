fe499b2fa1db25f84b8987c4fa2bab7a49d79e43
zodiuxus <22411434+zodiuxus@users.noreply.github.com>
2024-12-08 13:53:00 +0100

    Add files via upload

--- domasno1.pl
+++ domasno1.pl
@@ -37,4 +37,49 @@ proveri(L):-dolzina(L, N), N >= 2, prov_parovi(L).
 
 % Zadacha 4
 
+izberi_perm(X, [X|R], R).
+izberi_perm(X, [Prv|Opa], [Prv|Ost]):- izberi_perm(X, Opa, Ost).
 
+permutacii([], []).
+permutacii(L1, [X|L2]):- izberi_perm(X, L1, O), permutacii(O, L2).
+
+% Zadacha 5
+
+bin_dec([], 0).
+bin_dec([Bit|Ost], Dec):- bin_dec(Ost, OstDec), length(Ost, Stepen), Dec is Bit*2^Stepen+OstDec.
+
+dec_bin(Dec, Bin):- dec_bin(Dec, [], Bin).
+dec_bin(0, Temp, [0|Temp]).
+dec_bin(1, Temp, [1|Temp]).
+dec_bin(Dec, Temp, Bin):-
+  Dec > 1, 
+  Ost is Dec // 2, 
+  Bit is Dec mod 2, 
+  dec_bin(Ost, [Bit|Temp], Bin).
+
+sobiranje(Bin1, Bin2, Rezultat) :-
+    bin_dec(Bin1, Dec1),
+    bin_dec(Bin2, Dec2),
+    Sum is Dec1 + Dec2,
+    dec_bin(Sum, Rezultat).
+
+mnozhenje(Bin1, Bin2, Rezultat) :-
+    bin_dec(Bin1, Dec1),
+    bin_dec(Bin2, Dec2),
+    Proizvod is Dec1 * Dec2,
+    dec_bin(Proizvod, Rezultat).
+
+odzemanje(Bin1, Bin2, Rezultat) :-
+    bin_dec(Bin1, Dec1),
+    bin_dec(Bin2, Dec2),
+    Razlika is Dec1 - Dec2,
+    (   Razlika < 0
+    ->  Rezultat = [0]
+    ;   dec_bin(Razlika, Rezultat)
+    ).
+
+delenje(Bin1, Bin2, Rezultat) :-
+    bin_dec(Bin1, Dec1),
+    bin_dec(Bin2, Dec2),
+    Kolichnik is Dec1 // Dec2,
+    dec_bin(Kolichnik, Rezultat).
